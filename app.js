cc29ae45d62a5d7b625525be6c32dbbf9bd5ea03
anja <rasoloanja@gmail.com>
2020-07-06 15:51:45 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(bodyParser.json()); // application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
+  req.setRequestHeader("Accept", "*/*")
+  req.setRequestHeader('Access-Control-Allow-Origin', "*")
+  req.setRequestHeader('Allow-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+  req.setRequestHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization")
+  req.setRequestHeader("Content-Type", "application/json")
+  
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
