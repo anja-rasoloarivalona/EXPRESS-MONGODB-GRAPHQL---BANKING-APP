3500d8cba21d7e5d664879c18cb879905cb92462
anja <rasoloanja@gmail.com>
2020-07-09 11:28:59 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept');
-  if(req.method === 'OPTIONS'){
-    return res.sendStatus(200)
-  }
+  // if(req.method === 'OPTIONS'){
+  //   return res.sendStatus(200)
+  // }
   next();
 });
 
