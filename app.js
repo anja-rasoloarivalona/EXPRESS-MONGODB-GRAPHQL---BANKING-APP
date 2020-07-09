0706eb0a935e64479c27eb762d22469183640e62
anja <rasoloanja@gmail.com>
2020-07-08 22:27:06 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -14,12 +14,10 @@ import isAuth from './middleware/is-auth'
 
 const app = express();
 
+app.use(cors({origin: 'http://localhost:8080'}));
+
 app.use(helmet())
 // app.use(compression())
-
-app.use('*', cors());
-
-
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 // app.use(bodyParser.urlencoded({ extended: true }));
