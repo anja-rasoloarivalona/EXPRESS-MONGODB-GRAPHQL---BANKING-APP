4222d9b9fd48f4e220b42a16a08f0a3139f854bd
anja <rasoloanja@gmail.com>
2020-07-09 08:23:23 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ import isAuth from './middleware/is-auth'
 
 const app = express();
 
-app.use('*', cors());
+app.use(cors());
 
 // app.use(cors({origin: '*'}))
 
@@ -26,7 +26,7 @@ app.use(bodyParser.json()); // application/json
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/?#/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT, PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept');
   if(req.method === 'OPTIONS'){
