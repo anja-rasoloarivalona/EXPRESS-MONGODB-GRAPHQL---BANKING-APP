12a760aa1f8e95b0b4835ae4724ea84cd721245f
anja <rasoloanja@gmail.com>
2020-07-11 12:33:04 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('*', cors());
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -40,8 +40,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Max-Age", 86400)
   if(req.method == 'OPTIONS'){
     res.sendStatus(204)
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
   } else {
     next();
   }
