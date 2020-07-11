84ab61d77dc740aae69031f70805a9c5152f9d3e
anja <rasoloanja@gmail.com>
2020-07-11 12:04:14 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -33,13 +33,15 @@ app.use(bodyParser.json()); // application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
-  // res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  // res.header("Access-Control-Allow-Credentials", true);
-  // res.header("Access-Control-Max-Age", 86400)
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Max-Age", 86400)
   if(req.method == 'OPTIONS'){
     res.sendStatus(204)
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
   } else {
     next();
   }
