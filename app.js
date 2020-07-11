b987b0ddb43521b0d81e4b5790980c3fdc879ae7
anja <rasoloanja@gmail.com>
2020-07-11 11:44:00 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ import isAuth from './middleware/is-auth'
 
 const app = express();
 
-const corsOptions = {
-  origin: true, 
-  credentials: true
-}
-app.options('*', cors(corsOptions));
+// const corsOptions = {
+//   origin: true, 
+//   credentials: true
+// }
+app.use('*', cors());
 
 app.use(helmet())
 
@@ -28,11 +28,11 @@ app.use(bodyParser.json()); // application/json
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Max-Age", 86400)
+  // res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Max-Age", 86400)
   if(req.method === 'OPTIONS'){
     res.sendStatus(204)
   } else {
