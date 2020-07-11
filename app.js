461020bc81c00912166bdca36bdc6e27140730e8
anja <rasoloanja@gmail.com>
2020-07-11 12:37:18 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/graphql', graphHttp({
 mongoose
   .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-wijrw.mongodb.net/bank`, { useNewUrlParser: true, useUnifiedTopology: true } )
   .then(result => {
-    app.listen(process.env.PORT || 8000)
+    app.listen(process.env.PORT || 4001)
     console.log('connected')
   })
   .catch(err => console.log(err));
