3b04ba0981126a719d83bf864465f664aac9864d
anja <rasoloanja@gmail.com>
2020-08-15 23:19:12 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -12,35 +12,34 @@ import isAuth from './middleware/is-auth'
 
 const app = express();
 
+// const corsOptions = {
+//   origin: true, 
+//   credentials: true
+// }
+
 app.use(helmet())
 app.use(compression())
 
 app.use('*', cors());
+
+
+// app.use(cors({origin: '*'}))
+
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 // app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    console.log('options')
-    var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
-    return res.end()
-  } else {
-    console.log('else')
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept');
-    res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, DELETE, OPTIONS");
-    next()
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Max-Age", 86400)
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(204)
+  } 
+  next()
 });
 
 
